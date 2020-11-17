d5f8a31abcfad12cfa9d95e69df758a6cae47c50
Ann Xiong <AXiong@intercitytransit.com>
2020-11-16 21:36:06 -0800

    🛠️ Fixed main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import time
 prefix = input("Prefix: ")
 
 # Bee mode :)
-if input == "activatebeemode":
+if prefix == "beemode":
     beemode = input("Do you want to activate bee mode? Y/N: ")
     if beemode == "y" or beemode == "Y":
         f = open("beemovie", "r")
-        time.sleep(5)
+        print("Here's 10 seconds to change your mind :)")
+        time.sleep(10)
         for word in f:
             pyautogui.typewrite(word)
             pyautogui.press("enter")
@@ -31,9 +32,9 @@ while x > 0:
 while True:
     x = 0
     while x < times:
-        pyautogui.write(command)
+        pyautogui.write(prefix + command)
         pyautogui.press("enter")
         time.sleep(cooldown)
         x += 1
-    pyautogui.write(secondary)
+    pyautogui.write(prefix + secondary)
     pyautogui.press("enter")
