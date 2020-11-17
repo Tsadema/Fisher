e2d6699a1b147c7522a363ef49f7c4ddce0e4e24
Ann Xiong <AXiong@intercitytransit.com>
2020-11-16 16:20:14 -0800

    Added bee mode and bee movie script

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import pyautogui
 import time
+import json
 
-time.sleep(5)
+# Config
+prefix = input("Prefix: ")
 
+# Bee mode
+if input == "activatebeemode":
+    beemode = input("Do you want to activate bee mode? Y/N: ")
+    if beemode == y or beemode == Y:
+        f = open("beemovie", "r")
+        time.sleep(5)
+        for word in f:
+            pyautogui.typewrite(word)
+            pyautogui.press("enter")
+
+# Config: the sequel
+command = input("Command: ")
+secondary = input("Secondary Command (Sell command or empty for none): ")
+cooldown = float(input("Cooldown: "))
+times = int(input("Amount (Times to execute command before secondary command): "))
+
+# Warning before it starts
+x = 5
+while x > 0:
+    time.sleep(1)
+    print(f'Starting in {x}')
+    x -= 1
+
+# Program loop
 while True:
     x = 0
-    while x < 11:
-        pyautogui.write('%f')
+    while x < times:
+        pyautogui.write(command)
         pyautogui.press("enter")
-        time.sleep(3.5)
+        time.sleep(cooldown)
         x += 1
-    pyautogui.write('%sell all')
+    pyautogui.write(secondary)
     pyautogui.press("enter")
